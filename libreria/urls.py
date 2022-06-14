ee52f9b22ff37e12273781e5925259feae0f6d89
alexander <hs19011@ues.edu.sv>
2022-06-13 20:02:48 -0600

    conexion con base de datos psql

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,7 +1,13 @@
 ﻿from django.urls import path
 from . import views
 
+# enlaces para mostrar las vistas (URLS)
+
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('nosotros', views.nosotros, name='nosotros'),
+
+    path('libros', views.libros, name='libros'),
+    path('libros/crear', views.crear, name='crear'),
+    path('libros/editar', views.editar, name='editar'),
 ]

e22fc23cead27612dfec62553e97b4b5bcc3b559
alexander <hs19011@ues.edu.sv>
2022-06-14 00:29:57 -0600

    fin del tutorial

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Libro(models.Model):
+    id=models.AutoField(primary_key=True)
+    titulo=models.CharField(max_length=100, verbose_name='Titulo', null=True)
--- libreria/views.py
+++ libreria/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Libro
+from .forms import LibroForm
 # Create your views here.
 
 # acceso a los archivos (paginas)
@@ -7,13 +9,31 @@ from django.http import HttpResponse
 # paginas del sitio
 def inicio(request):
     return render(request, 'paginas/inicio.html')
+
 def nosotros(request):
     return render(request, 'paginas/nosotros.html')
 
 #libros
 def libros(request):
-    return render(request, 'libros/index.html')
+    libros = Libro.objects.all()
+    return render(request, 'libros/index.html', {'libros': libros})
+
 def crear(request):
-    return render(request, 'libros/crear.html')
-def editar(request):
-    return render(request, 'libros/editar.html')
+    formulario = LibroForm(request.POST or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/crear.html', {'formulario': formulario})
+
+def editar(request, id):
+    libro = Libro.objects.get(id=id)
+    formulario = LibroForm(request.POST or None, instance=libro)
+    if formulario.is_valid() and request.POST:
+        formulario.save()
+        return redirect('libros')
+    return render(request, 'libros/editar.html', {'formulario': formulario})
+
+def eliminar(request, id):
+    libro = Libro.objects.get(id=id)
+    libro.delete()
+    return redirect('libros')
